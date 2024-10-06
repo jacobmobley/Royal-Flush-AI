e975c8058da9393cc095f0847893d445c653fffe
AndreiDea <adeacone@purdue.edu>
2024-10-06 17:11:12 -0500

    fixed firebase auth

--- react-frontend/src/firebase.js
+++ react-frontend/src/firebase.js
@@ -2,14 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-        apiKey: "AIzaSyAB17cAEAazgHGbj8QBG8SJ0yE-gyPNnuo",
-        authDomain: "royalflushai-database.firebaseapp.com",
-        projectId: "royalflushai-database",
-        storageBucket: "royalflushai-database.appspot.com",
-        messagingSenderId: "1013082457124",
-        appId: "1:1013082457124:web:a413a98845e23f3b1dc4b1",
-        measurementId: "G-J0V98K5DRD"
-    };
+  apiKey: "AIzaSyAB17cAEAazgHGbj8QBG8SJ0yE-gyPNnuo",
+  authDomain: "royalflushai-database.firebaseapp.com",
+  projectId: "royalflushai-database",
+  storageBucket: "royalflushai-database.appspot.com",
+  messagingSenderId: "1013082457124",
+  appId: "1:1013082457124:web:a413a98845e23f3b1dc4b1",
+  measurementId: "G-J0V98K5DRD"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+
+export { auth };
