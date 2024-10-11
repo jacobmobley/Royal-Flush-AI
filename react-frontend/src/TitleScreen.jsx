68b4763a2f47b6f7bc2565e3d30da98693a160d3
jacobmobley <mobley6@purdue.edu>
2024-10-10 21:14:48 -0400

    fixed wierd merge thingy

--- react-frontend/src/TitleScreen.jsx
+++ react-frontend/src/TitleScreen.jsx
@@ -100,38 +100,3 @@ const TitleScreen = () => {
     </div>
   );
 };
-
-export default TitleScreen;
-=======
-import React from "react";
-import styles from "./frontpage-styles.module.css"; // Import CSS module
-import logo from "./assets/RoyalFlushAILogo.png";
-import { Link } from "react-router-dom";
-
-const TitleScreen = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.logoContainer}>
-        <img src={logo} alt="Royal Flush AI Logo" className={`${styles.logo}`} />
-      </div>
-      <div className={styles.buttonContainer}>
-        <h1>Royal Flush AI</h1>
-        {/* Link for New Player page */}
-        <Link to="/newplayer" className={`${styles.button} ${styles.newPlayer}`}>
-          New Player? <br />
-          Create a New Account
-        </Link>
-        {/* Link for Returning Player page */}
-        <Link to="/returningplayer" className={`${styles.button} ${styles.returningPlayer}`}>
-          Returning Player? <br />
-          Sign into Existing Account
-        </Link>
-        <Link to="/homepage" className={`${styles.button} ${styles.preview}`}>
-          Preview Title Screen
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-export default TitleScreen;
