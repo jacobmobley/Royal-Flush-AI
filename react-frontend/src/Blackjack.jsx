c9c194b36587acaba540ce749a0e22634de34dd5
Andrew Ge <ge91@purdue.edu>
2024-10-27 17:11:42 -0400

    Fixed some bugs in the css
    Some of the colors and buttons were messed up between modules

--- react-frontend/src/Blackjack.jsx
+++ react-frontend/src/Blackjack.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import styles from './Blackjack.module.css';
-import FireBaseAuth from './FireBaseAuth';
+import React, { useState, useEffect } from "react";
+import styles from "./Blackjack.module.css";
+import FireBaseAuth from "./FireBaseAuth";
 
 const suits = ["♠", "♣", "♦", "♥"];
 const values = [
