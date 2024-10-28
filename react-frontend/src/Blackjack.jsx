dd38ff1191d999dfeac69af789327a35d71a5448
raymondwang037 <raymondwang037@gmail.com>
2024-10-27 20:53:43 -0400

    Multi-deck blackjack done

--- react-frontend/src/Blackjack.jsx
+++ react-frontend/src/Blackjack.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
-import styles from "./Blackjack.module.css";
-import FireBaseAuth from "./FireBaseAuth";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './Blackjack.module.css';
+import FireBaseAuth from './FireBaseAuth';
 
 const suits = ["♠", "♣", "♦", "♥"];
 const values = [
