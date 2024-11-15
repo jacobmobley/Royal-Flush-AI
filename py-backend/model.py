fafafc92a84952e429bbf7f8a64938e0d9de5f6e
raymondwang037 <raymondwang037@gmail.com>
2024-11-14 20:52:00 -0500

    fixed path issues

--- py-backend/model.py
+++ py-backend/model.py
@@ -4,8 +4,8 @@ import pickle
 import joblib
 import os
 
-with open(os.path.abspath('py-backend/poker_ai_model.pkl'), 'rb') as model_file:
+with open(os.path.abspath('poker_ai_model.pkl'), 'rb') as model_file:
     model : RandomForestRegressor = pickle.load(model_file)
 
-with open('py-backend/serializer.pkl', 'rb') as scaler_file:
+with open('serializer.pkl', 'rb') as scaler_file:
     scaler : MinMaxScaler = pickle.load(scaler_file)
